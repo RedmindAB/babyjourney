dfd464397b9f4fd78afa1883d2d93d881c18daa6
Lukas Klingberg <lukas@klingberg.se>
2019-09-16 14:16:23 +0200

    added modal translations

--- app/components/common/WeekCongrats/WeekCongrats.tsx
+++ app/components/common/WeekCongrats/WeekCongrats.tsx
@@ -9,6 +9,7 @@ import theme from '../../../theme'
 import { connect } from 'react-redux'
 import { ApplicationState } from '../../../store'
 import { getWeekAndDay } from '../../../utils'
+import i18n, { lang } from '../../../translations'
 
 type PropsFromState = ReturnType<typeof mapStateToProps>
 
@@ -19,19 +20,19 @@ class WeekCongrats extends Component<Props> {
     const { weeks } = getWeekAndDay(this.props.user.dueDate)
     return (
       <WeekCongratsContainer>
-        <Headline>Welcome to</Headline>
-        <Title>Week {weeks}</Title>
+        <Headline>{i18n.translate(lang.weekCongratsModal.title)}</Headline>
+        <Title>{i18n.translate(lang.weekCongratsModal.subTitle, { week: weeks })}</Title>
         <ConfettiContainer>
           <Icons.Confetti />
         </ConfettiContainer>
         <InfoText style={{ fontSize: 16, fontWeight: '400', marginBottom: theme.BASELINE * 4 }}>
-          Congartulations! This is how far you have progressed in your pregnancy
+          {i18n.t(lang.weekCongratsModal.congratulationsText)}
         </InfoText>
         <BabyPercentage />
         <SquareButton
           bigButton
           style={{ width: 160, marginTop: theme.BASELINE * 4 }}
-          title="share"
+          title={i18n.t(lang.weekCongratsModal.buttonText)}
           onPress={() => undefined}
         />
       </WeekCongratsContainer>
